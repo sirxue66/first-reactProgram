ecbd0b4058545e1dc826239b763ab2a9c6697ca3
薛成 <xuechengcheng@sinovatech.com>
2021-08-19 18:32:36 +0800

    引入redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import {BrowserRouter,Switch,Route} from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Register from './pages/register/register'
+import Login from './pages/login/login'
+import Main from './pages/main/main'
+class App extends Component{
+    render(){
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/register" component={Register}></Route>
+                    <Route path="/login" component={Login}></Route>
+                    <Route component={Main}></Route>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
 }
 
-export default App;
+export default App
