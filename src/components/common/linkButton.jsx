d8d33e35e43e7aa6a8010102611b2dba3b092bc2
薛成 <xuechengcheng@sinovatech.com>
2021-09-24 16:57:38 +0800

    添加接口

---
+++ src/components/common/linkButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./linkButton.less"
+
+export default function LinkButton(props){
+    return(
+        <button {...props} className="linkButton"></button>
+    )
+}
